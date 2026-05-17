2ea13602957ba94e3f58a206278fd82a2cc2f175
6ix <Ukcharlies@gmail.com>
2026-05-17 07:32:33 +0100

    updateed the yaml files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactCompiler: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
