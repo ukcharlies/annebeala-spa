38f3f60a00798c1d654f010d900bde7682e3fcd8
6ix <Ukcharlies@gmail.com>
2026-07-23 16:03:47 +0100

    fix

--- frontend/src/lib/availability.ts
+++ frontend/src/lib/availability.ts
@@ -1,4 +1,4 @@
-export const serverFeeUnpaid = false; // should be false
+export const serverFeeUnpaid = true; // should be false
 export const supportFeeUnpaid = false; //should be false
 
 export const siteUnavailable = serverFeeUnpaid || supportFeeUnpaid;
